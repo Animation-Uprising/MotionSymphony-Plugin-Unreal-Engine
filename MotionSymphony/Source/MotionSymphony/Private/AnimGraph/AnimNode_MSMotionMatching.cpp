1ce7dc6748d6ce3067c7393de53235d58d294e8a
Kenneth Claassen <claassen.ken@gmail.com>
2023-11-25 10:26:03 +1000

    Potential Crash fix on motion matching node
    - Fixed a crash caused if a MotionAnim somehow becomes null.

--- MotionSymphony/Source/MotionSymphony/Private/AnimGraph/AnimNode_MSMotionMatching.cpp
+++ MotionSymphony/Source/MotionSymphony/Private/AnimGraph/AnimNode_MSMotionMatching.cpp
@@ -867,7 +867,7 @@ void FAnimNode_MSMotionMatching::JumpToPose(const int32 PoseIdDatabase, const fl
 		{
 			TObjectPtr<const UMotionSequenceObject> MotionAnim = CurrentMotionData->GetSourceSequenceAtIndex(Pose.AnimId);
 
-			if (!MotionAnim->Sequence)
+			if (!MotionAnim || !MotionAnim->Sequence)
 			{
 				break;
 			}
@@ -881,7 +881,7 @@ void FAnimNode_MSMotionMatching::JumpToPose(const int32 PoseIdDatabase, const fl
 		{
 			TObjectPtr<const UMotionBlendSpaceObject> MotionBlendSpace = CurrentMotionData->GetSourceBlendSpaceAtIndex(Pose.AnimId);
 
-			if (!MotionBlendSpace->BlendSpace)
+			if (!MotionAnim || !MotionBlendSpace->BlendSpace)
 			{
 				break;
 			}
@@ -899,7 +899,7 @@ void FAnimNode_MSMotionMatching::JumpToPose(const int32 PoseIdDatabase, const fl
 		{
 			TObjectPtr<const UMotionCompositeObject> MotionComposite = CurrentMotionData->GetSourceCompositeAtIndex(Pose.AnimId);
 
-			if (!MotionComposite->AnimComposite)
+			if (!MotionAnim || !MotionComposite->AnimComposite)
 			{
 				break;
 			}
